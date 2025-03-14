742f04d2af2dc257774d362a7c77b74ace85e000
hajalimazen__ <mazen.hadjali@episousse.com.tn>
2025-03-14 14:22:20 +0100

    fix validator params

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,19 +3,19 @@ export interface FormDefinition {
     name?: string;
     properties: {
         [key: string]:
-        | { type: 'text', title: string, description: string, required?: boolean, validator?: () => boolean }
-        | { type: 'password', title: string, description: string, required?: boolean, validator?: () => boolean }
-        | { type: 'email', title: string, description: string, required?: boolean, validator?: () => boolean }
-        | { type: 'date', title: string, description: string, validator?: () => boolean }
-        | { type: 'time', title: string, description: string, required?: boolean, validator?: () => boolean }
-        | { type: 'string', title: string, description: string, required?: boolean, validator?: () => boolean }
-        | { type: 'select', title: string, description: string, options: { label: string, value: string }[], required?: boolean, validator?: () => boolean }
-        | { type: 'radio', title: string, description: string, options: { label: string, value: string }[], required?: boolean, validator?: () => boolean }
-        | ({ type: 'number', title: string, description: string, required?: boolean, validator?: () => boolean } & (
+        | { type: 'text', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'password', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'email', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'date', title: string, description: string, validator?: (value: any) => boolean }
+        | { type: 'time', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'string', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'select', title: string, description: string, options: { label: string, value: string }[], required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'radio', title: string, description: string, options: { label: string, value: string }[], required?: boolean, validator?: (value: any) => boolean }
+        | ({ type: 'number', title: string, description: string, required?: boolean, validator?: (value: any) => boolean } & (
             | { minimum?: never, maximum?: never }
             | { minimum: number, maximum: number }
         ))
-        | { type: 'boolean', title: string, description: string, required?: boolean, validator?: () => boolean }
-        | { type: 'checkbox', title: string, description: string, required?: boolean, validator?: () => boolean }
+        | { type: 'boolean', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
+        | { type: 'checkbox', title: string, description: string, required?: boolean, validator?: (value: any) => boolean }
     };
 }
